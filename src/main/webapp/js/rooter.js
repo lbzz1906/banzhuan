00a75dd5eb9a8cb56595e83510f245e21bec49b8
找不到宇航员 <lbzz1906@qq.com>
2020-08-14 16:21:18 +0800

    2.1web

--- src/main/webapp/js/rooter.js
+++ src/main/webapp/js/rooter.js
@@ -1,6 +1,9 @@
 var nam = document.getElementById('nam');
 var pri = document.getElementById('pri');
 var but = document.getElementById('but');
+var num = document.getElementById('num');
+var des = document.getElementById('des');
+var shop = document.getElementById('shop');
 var tbody = document.querySelector('tbody');
 
 var em = [];
@@ -8,29 +11,34 @@ var datas = [{}];
 but.addEventListener('click', function () {
     var namv = nam.value;
     var priv = pri.value;
-
+    var numv = num.value;
+    var desv = des.value;
+    var shopv = shop.value;
     $.getJSON("/addGoods",
-        {"goodsName":namv,"goodsPrice":priv},
+        {"goodsName": namv, "goodsPrice": priv},
         function (data) {
-            if(data){
+            if (data) {
                 alert("请添加图片");
-            }else {
+            } else {
                 alert("添加失败");
             }
         }
-        )
+    )
 
-    em = [{name: namv, pri: priv}];
-    nam.value='';
-    pri.value='';
-    if(namv.length===0){
+    em = [{num: numv, name: namv, des: desv, shop: shopv, pri: priv}];
+    nam.value = '';
+    pri.value = '';
+    num.value = '';
+    des.value = '';
+    shop.value = '';
+    if (namv.length === 0) {
         alert('请输入名称');
         return;
-    }else {
-        if(priv.length===0){
+    } else {
+        if (priv.length === 0) {
             alert('请输入价格');
             return;
-        }else{
+        } else {
             for (var i = 0; i < em.length; i++) {
                 datas.push(em[i]);
             }
@@ -39,7 +47,7 @@ but.addEventListener('click', function () {
 
     var tr = document.createElement('tr');
     var li = document.createElement("b");
-    li.innerHTML='<img src="" height="200" alt="img..."><br>';
+    li.innerHTML = '<img src="" height="200" alt="img..."><br>';
 
     tbody.appendChild(tr);
     tr.appendChild(li);
@@ -52,11 +60,11 @@ but.addEventListener('click', function () {
     var del = document.createElement('td');
     del.innerHTML = '<a id="dell" href="javascript:;">删除</a>';
     tr.appendChild(del);
-    return datas.length-1;
+    return datas.length - 1;
 })
 var as = document.querySelectorAll('a');
 for (var i = 0; i < as.length; i++) {
-    as[i].onclick = function() {
+    as[i].onclick = function () {
         // node.removeChild(child); 删除的是 li 当前a所在的li  this.parentNode;
         alert("vbnm")
     }
@@ -65,27 +73,27 @@ for (var i = 0; i < as.length; i++) {
 function previewFile() {
     const preview = document.querySelectorAll('img');
     const file = document.querySelector('input[type=file]').files[0];
-    var formData=new FormData();
-    formData.append('file',file);
+    var formData = new FormData();
+    formData.append('file', file);
     const reader = new FileReader();
-    var btn=document.querySelector(".btns");
-    var inp=document.querySelectorAll("input");
+    var btn = document.querySelector(".btns");
+    var inp = document.querySelectorAll("input");
     reader.addEventListener("load", function () {
-        preview[preview.length-1].src = reader.result;
+        preview[preview.length - 1].src = reader.result;
 
         $.ajax({
             type: 'post',
             url: "http://localhost:8080/addImage",
             data: formData,
-            dataType:'json',
-            async:false,
+            dataType: 'json',
+            async: false,
             cache: false,
-            contentType:false,
-            processData:false,
+            contentType: false,
+            processData: false,
             success: function (data) {
                 console.log(data)
             },
-            error:function (err) {
+            error: function (err) {
                 console.log(err)
             }
         });
