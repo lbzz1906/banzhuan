f00b5907e41438545998112582ff6fef454cd31b
找不到宇航员 <lbzz1906@qq.com>
2020-08-16 11:50:09 +0800

    goods2.14 web

---
+++ src/main/webapp/js/goods.js
@@ -0,0 +1,6 @@
+var num=document.querySelector('#num');
+var nam=document.querySelector('#nam');
+var pri=document.querySelector('#pri');
+var time=document.querySelector('#time');
+var shop=document.querySelector('#shop');
+var img=document.querySelector('img');
